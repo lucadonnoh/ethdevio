f3647d8a13b5055cfbea4000f80f3520f3337b56
Luca Donno <donnoh@MacBook-Air-di-Luca.local>
2020-10-28 09:17:15 +0100

    ValueChanged event

--- Counter.sol
+++ Counter.sol
@@ -2,6 +2,8 @@
 pragma solidity 0.7.0;
 
 contract Counter {
+
+    event ValueChanged(uint oldValue, uint newValue);
     
     // Public variable of type unsigned int to keep the number of counts
     uint256 public count = 0;
@@ -9,6 +11,7 @@ contract Counter {
     // Function that increments our counter
     function increment() public {
         count += 1;
+        emit ValueChanged(count - 1, count);
     }
     
     // Not necessary getter to get the count value
